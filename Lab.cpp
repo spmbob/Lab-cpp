f61e2f4ccfa538082379e88a12f7a405568cb47b
spmbob <urban_manki@mail.ru>
2016-12-15 18:09:00 +0300

    Lab cpp

---
+++ Lab.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int myfunc(int a,int b,int v) {
+    int umn;
+    umn=a*b*v;
+    return umn;
+ }int y,int m,int k
+ int myfunc(int g,int d,int e) {
+    int summa;
+    umn=a+b+v;
+    return summa;
+ }
+ int main() {
+    int a,b,v,umn;
+    cout<<"a: "<<endl;
+    cin>>a;
+    cout<<"b: "<<endl;
+    cin>>b;
+    cout<<"v: "<<endl;
+    cin>>v;
+    umn = myfunc(a,b,v);
+    cout<<umn<<endl;
+    cout<<v<<endl;    
+    system ("pause");
+    return 0;
+ }
+ 
+
---
+++ Labbb .cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a;
+   cout<<"Number: "<<endl;
+   cin>>a;
+   cout<<"Number: "<<a<<endl;
+   system ("pause");
+   return 0;
+}
